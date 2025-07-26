376f8c89502dfc4e63613d3ba53ed0786b24a18c
dhivyarrk <dhivyabharathi.radhakrishnanr@gmail.com>
2025-07-26 11:38:24 +0300

    Added humburger and mobile menu in html, css and jaavascript

--- javascript/main.js
+++ javascript/main.js
@@ -26,3 +26,19 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+const hamburger = document.getElementById("hamburger");
+const mobileMenu = document.getElementById("mobileMenu");
+
+hamburger.addEventListener("click", () => {
+  mobileMenu.classList.toggle("show");
+});
+
+// Optional: Close menu when a link is clicked
+mobileMenu.querySelectorAll("a").forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.remove("show");
+  });
+});
+
+
